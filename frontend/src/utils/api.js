3feeae91a527d3ed733e1df78dc3d69a8d48e9c1
Gaurav0405 <jain.gaurav0405@gmail.com>
2026-05-19 16:07:06 +0530

    Update API URL for production

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api'
+  baseURL: 'http://65.0.72.242/api'
 });
 
 // Automatically attach token to every request
