0562a2c33b299373111f8b097fef0a7021eb3e56
Gaurav0405 <jain.gaurav0405@gmail.com>
2026-06-08 11:03:28 +0530

    Action whatever

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:5000/api'
+  baseURL: window.location.hostname === 'localhost'
+    ? 'http://localhost:5000/api'
+    : '/api'
 });
 
 api.interceptors.request.use((config) => {
